4754cb1e7d82d965beb63df3aec3e53379c48045
zacharyramos <zachary.ramos9@gmail.com>
2018-03-27 08:38:27 -0500

    servlet exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet (name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String name = req.getParameter("name");
+        if(name != null) {
+            name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+            out.println("<h1>Hello, " + name + "!</h1>");
+        } else {
+            out.println("<h1>Hello, World!</h1>");
+        }
+        out.println("<form>");
+        out.println("Name: ");
+        out.println("<input name = 'name'>");
+        out.println("</form>");
 
     }
 }
